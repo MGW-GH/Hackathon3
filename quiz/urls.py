6f61daa106c60b04c13df56167426de91b2cdea2
Charles Mould <charles.mould.cm@gmail.com>
2024-10-08 13:34:18 +0000

    add homepage view url

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
+from quiz.views import my_quiz, HomePage
 
 urlpatterns = [
-    path('', 
-        views.HomePage.as_view(), name='home'),
+    path('', views.HomePage.as_view(), name='home'),
 ]

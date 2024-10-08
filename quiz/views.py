e02a013d4e066db20cfead3df18dc60ec701f9b7
Charles Mould <charles.mould.cm@gmail.com>
2024-10-08 13:13:47 +0000

    ready for deployment, missing sql DB info due to server outage

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def my_quiz(request):
     return HttpResponse("Lets build a great, fun, quiz app!")
+
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    """
+    Displays home page"
+    """
+    template_name = 'index.html'

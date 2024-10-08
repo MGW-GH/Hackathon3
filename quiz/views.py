9a06b4566911a8c3264b4cff42c1023a98ba9dad
Charles Mould <charles.mould.cm@gmail.com>
2024-10-08 13:34:47 +0000

    remove repeated line of code

--- quiz/views.py
+++ quiz/views.py
@@ -6,7 +6,6 @@ from django.views.generic import TemplateView
 def my_quiz(request):
     return HttpResponse("Lets build a great, fun, quiz app!")
 
-from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
     """

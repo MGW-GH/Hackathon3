3ae8ee51101f70af29a435e69a77556521e0bb61
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-10-09 21:57:03 +0000

    fix: score now calculates properly and generates modal on submit

--- quiz/views.py
+++ quiz/views.py
@@ -1,31 +1,52 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Multiple_choice_trivia
-
 from . forms import UserRegistrationForm, UserLoginForm
 
-# Create your views here.
-# class QuizPage(TemplateView):
-#     """
-#     Displays quiz page
-#     """
-#     template_name = 'quiz.html'
- 
-
-# def QuizPage(request):
-    
-#     questions = Multiple_choice_trivia.objects.all()
-#     context = {'products':products, 'cartItems':cartItems}
-    
-#     return render(request, 'quiz/templates/quiz.html', context)
 
 def QuizPage(request):
-    questions = Multiple_choice_trivia.objects.all().order_by('?')[:5]  # Get 5 random questions
-    context = {'questions': questions}
+    if request.method == 'POST':
+        questions = Multiple_choice_trivia.objects.filter(question__in=request.POST.getlist('question_id'))
+        score = 0
+
+        for question in questions:
+            selected_answer = request.POST.get(f'q-{question.question}')
+            if selected_answer == 'A':
+                selected_answer = 0
+            if selected_answer == 'B':
+                selected_answer = 1
+            if selected_answer == 'C':
+                selected_answer = 2
+            if selected_answer == 'D':
+                selected_answer = 3
+
+            if selected_answer == question.answer:
+                score += 1
+        
+        message = get_score_message(score)
+        
+        return JsonResponse({'score': score, 'message': message})
+
+    else:
+        questions = Multiple_choice_trivia.objects.all().order_by('?')[:5]
+        return render(request, 'quiz.html', {'questions': questions})
+
+
+def get_score_message(score):
+    if score == 5:
+        return "Perfect score! Excellent job!"
+    elif score == 4:
+        return "Great job! Almost perfect!"
+    elif score == 3:
+        return "Good effort! You're on the right track."
+    elif score == 2:
+        return "Not bad, but there's room for improvement."
+    else:
+        return "Keep practicing, you'll get better!"
     
     return render(request, 'quiz.html', context)
 

7b2da022feb2d3a7c89387a4012dfef2c9d88094
Charles Mould <charles.mould.cm@gmail.com>
2024-10-09 15:05:56 +0000

    add: urls for login, logout and user success. edit: path for user registration

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,13 @@
 from . import views
 from django.urls import path
-from quiz.views import QuizPage, HomePage, UserRegistration
+#from django.contrib.auth import views as auth_views
+from quiz.views import QuizPage, HomePage, UserRegistrationView, UserSuccessView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    # Registration Page
-    path("UserRegistration/", UserRegistration.as_view(), name="UserRegistration"),
-
+    # Account related urls
+    path("register/", UserRegistrationView.as_view(), name="user_registration"),
+    path("registration/success/", UserSuccessView.as_view(), name="user_registration_success"),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
